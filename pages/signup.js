ec62e428329c7beca94ee78c0a5bbd25db6e2846
marwa <marwajawad19@gmail.com>
2021-09-30 15:40:58 +0300

    adding firebase

---
+++ pages/signup.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+// import firebase from '../firebase/clientApp';
+// import {
+//   getAuth,
+//   GoogleAuthProvider,
+//   signInWithRedirect,
+//   signInWithPopup,
+// } from 'firebase/auth';
+// const uiConfig = {
+//   signInSuccessUrl: '/',
+//   signInOptions: [GoogleAuthProvider],
+// };
+
+// function SignInScreen() {
+//   return (
+//     <div>
+//       <h1> Login</h1>
+//       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={getAuth} />
+//     </div>
+//   );
+// }
+
+// export default SignInScreen;
